728809b6ee086bf71dc785088f73a0b8bc681106
Kimseohyeon <seohyeon0219@naver.com>
2025-07-25 01:54:45 +0900

    Feat: login page 오른쪽 흰 영역 로그인 구현

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,28 +1,52 @@
+import LoginForm from "../../components/ui/auth/loginForm";
+import Button from "../../components/ui/auth/button";
+
 export default function LoginPage() {
     return (
         <div className="min-h-screen flex justify-center items-center bg-login-gray">
-            <div className="w-[650px] h-[400px] bg-white rounded-3xl">
-                {/* 네이비 박스 내 */}
-                <div className="w-[300px] h-[400px] bg-login-navy rounded-l-3xl flex flex-col p-3">
+            <div className="flex w-[700px] h-[500px] bg-white rounded-3xl">
+                {/* 왼쪽 네이비 박스 */}
+                <div className="w-[300px] h-[500px] bg-login-navy rounded-l-3xl flex flex-col p-6">
                     {/* 상단 로고 + 간단한 설명 */}
                     <div className="flex flex-col items-end">
                         <img src="../../../logos/logoWhite.png" alt="흰색 로고" width={150}></img>
-                        <p className="text-sm text-white">AI 기계 이상음 감지 시스템</p>
+                        <p className="text-xs text-white">AI 기계 이상음 감지 시스템</p>
                     </div>
                     {/* 가운데 로봇 사진 + 그레이 배경 */}
                     <div className="flex-1 relative">
                         {/* 어두운 그레이 배경 원 */}
                         <img src="../../../images/darkGrayCircle.svg" className="absolute w-72 h-40 bottom-10 left-0 z-0"></img>
                         {/* 밝은 그레이 배경 원 */}
-                        <img src="../../../images/lightGrayCircle.svg" className="absolute w-56 h-24 bottom-10 left-12 z-10"></img>
+                        <img src="../../../images/lightGrayCircle.svg" className="absolute w-52 h-24 bottom-10 left-10 z-10"></img>
                         {/* 로봇 이미지 */}
-                        <img src="../../../images/robot.png" className="absolute w-36 h-36 bottom-10 left-20 z-20"></img>
+                        <img src="../../../images/robot.png" className="absolute w-36 h-36 bottom-10 left-14 z-20"></img>
                     </div>
                     {/* 홍보웹 링크 */}
                     <div>
                         <p className="text-white text-xs">아래 링크로 문의바랍니다.</p>
                         <p className="text-white text-xs">홍보웹 이동 링크</p>
                     </div>
+                </div>
+                {/* 오른쪽 흰색 박스 */}
+                <div className="flex flex-1 flex-col justify-center items-center gap-6">
+                    {/* 상단 로그인 */}
+                    <div>
+                        <h1 className="text-2xl font-black">LOGIN</h1>
+                    </div>
+                    {/* input 컴포넌트 */}
+                    <div className="space-y-4">
+                        <LoginForm
+                            label = "사원번호"
+                            placeholder="사원번호를 입력하세요."
+                            iconname="person"
+                        />
+                        <LoginForm
+                            label = "비밀번호"
+                            placeholder="비밀번호를 입력하세요."
+                            iconname="lock"
+                        />
+                    </div>
+                    <Button>로그인</Button>
                 </div>
             </div>
         </div>
