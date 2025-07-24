6cc5ede7c88adb8c57f86bbcfe4b8e93748209db
Kimseohyeon <seohyeon0219@naver.com>
2025-07-25 01:39:07 +0900

    Feat: Google Material Icon 컴포넌트 추가

---
+++ src/components/ui/icon.tsx
@@ -0,0 +1,32 @@
+type IconProps = {
+    iconname: string;
+    size?: string | number;
+    color?: string;
+    weight?: number;
+    fill?: 0 | 1;
+    className?: string;
+};
+
+const Icon = ({ 
+    iconname, 
+    size = 18, 
+    color = "#B9B9B9", 
+    weight = 400, 
+    fill = 0, 
+    className = ''
+}: IconProps) => {
+    return (
+        <span 
+            className={`material-symbols-outlined ${className}`} 
+            style={{ 
+                fontSize: size,
+                color: color,    
+                fontVariationSettings: `'FILL' ${fill}, 'wght' ${weight}, 'GRAD' 0, 'opsz' 24`
+ }}
+        >
+            {iconname}
+        </span>
+    )
+}
+
+export default Icon;
