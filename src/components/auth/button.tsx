788b9889ae14b93520d110a93fa355edf4b58a95
Kimseohyeon <seohyeon0219@naver.com>
2025-07-25 14:42:55 +0900

    Feat: button 컴포넌트에 onClick 이벤트 추가

--- src/components/auth/button.tsx
+++ src/components/auth/button.tsx
@@ -1,14 +1,19 @@
 interface ButtonProps {
     children: React.ReactNode;
     className?: string;
+    onClick?: () => void;
 }
 
 export default function Button({
     children,
-    className = ""
+    className = "",
+    onClick
 }: ButtonProps) {
     return (
-        <button className={`w-72 h-12 btn-login-gray text-white cursor-pointer ${className}`}>
+        <button 
+            className={`w-72 h-12 btn-login-gray text-white cursor-pointer ${className}`}
+            onClick={onClick}    
+        >
             {children}
         </button>
     )
