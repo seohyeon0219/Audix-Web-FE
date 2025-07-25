8144d10e49790b27d4d3cd160476009c858f9fdb
Kimseohyeon <seohyeon0219@naver.com>
2025-07-25 15:13:21 +0900

    Feat: img 태그에 alt 속성 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -20,17 +20,17 @@ export default function LoginPage() {
                 <div className="w-[300px] h-[500px] bg-login-navy rounded-l-3xl flex flex-col p-6 pt-10">
                     {/* 상단 로고 + 간단한 설명 */}
                     <div className="flex flex-col items-end">
-                        <img src="../../../logos/logoWhite.png" alt="흰색 로고" width={150}></img>
+                        <img src="../../../logos/logoWhite.png" alt="audix white logo" width={150}></img>
                         <p className="text-xs text-white">AI 기계 이상음 감지 시스템</p>
                     </div>
                     {/* 가운데 로봇 사진 + 그레이 배경 */}
                     <div className="flex-1 relative">
                         {/* 어두운 그레이 배경 원 */}
-                        <img src="../../../images/darkGrayCircle.svg" className="absolute w-72 h-40 bottom-10 left-0 z-0"></img>
+                        <img src="../../../images/darkGrayCircle.svg" alt="dark gray circle" className="absolute w-72 h-40 bottom-10 left-0 z-0"></img>
                         {/* 밝은 그레이 배경 원 */}
-                        <img src="../../../images/lightGrayCircle.svg" className="absolute w-52 h-24 bottom-10 left-10 z-10"></img>
+                        <img src="../../../images/lightGrayCircle.svg" alt="light gray circle" className="absolute w-52 h-24 bottom-10 left-10 z-10"></img>
                         {/* 로봇 이미지 */}
-                        <img src="../../../images/robot.png" className="absolute w-36 h-36 bottom-10 left-14 z-20"></img>
+                        <img src="../../../images/robot.png" alt="robot" className="absolute w-36 h-36 bottom-10 left-14 z-20"></img>
                     </div>
                     {/* 홍보웹 링크 */}
                     <div>
