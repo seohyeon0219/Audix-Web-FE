833cde91f82183304f2759c2f43b319804de6d3f
Kimseohyeon <seohyeon0219@naver.com>
2025-07-28 17:11:40 +0900

    Feat: 전 구역 모니터링, searchBar 기능 구현

--- src/app/area/page.tsx
+++ src/app/area/page.tsx
@@ -1,12 +1,15 @@
 import { getStatusFromValue, getStatusColor } from "../../constants/status";
 
-interface recentDashboardData {
+import SearchBar from "../../components/common/searchBar";
+
+// 최근 10일 대시보드
+interface RecentDashboardData {
     date: string;
     value: number; // 0.1 ~ 1.0
 }
 
 // mock data
-const mockRecentDashboardData: recentDashboardData[] = [
+const mockRecentDashboardData: RecentDashboardData[] = [
     { date: '2025-01-01', value: 0.9 },
     { date: '2025-01-02', value: 0.8 },
     { date: '2025-01-03', value: 0.7 },
@@ -19,13 +22,45 @@ const mockRecentDashboardData: recentDashboardData[] = [
     { date: '2025-01-10', value: 0.1 },
 ]
 
+// 이번 달 알림 건수 
+type DateAlarmType = 'danger' | 'warning';
+
+interface DataAlarmCount {
+    danger: number;
+    warning: number;
+}
+
+// mock data
+const dateAlarmCounts: DataAlarmCount = {
+    danger: 1,
+    warning: 5
+}
+
+const alarms = [
+    {
+        key: 'danger' as DateAlarmType,
+        label: '위험',
+        count: dateAlarmCounts.danger,
+        textColor: 'text-danger',
+        borderColor: 'border-danger'
+    },
+    {
+        key: 'warning' as DateAlarmType,
+        label: '점검\n요망',
+        count: dateAlarmCounts.warning,
+        textColor: 'text-warning',
+        borderColor: 'border-warning'
+    }
+]
+
+
 export default function AreaPage() {
     return (
         <div>
             {/* 윗 부분 */}
-            <div className="flex gap-10">
+            <div className="flex gap-10 mb-10">
                 {/* 최근 10일 대시보드 */}
-                <div className="bg-main-100 min-w-80 w-fit rounded-lg px-14 py-4 border border-t-white">
+                <div className="bg-main-100 min-w-96 w-fit rounded-lg px-14 py-4 border border-t-white">
                     <h3 className="text-white pb-2 mb-4 border-b border-white">최근 10일 대시보드</h3>
                     <div className="flex gap-3">
                         {mockRecentDashboardData.map((data, index) => {
@@ -43,7 +78,40 @@ export default function AreaPage() {
 
                     </div>
                 </div>
+                {/* 이번 달 알람 건수 */}
+                <div className="flex items-center gap-8 bg-main-100 min-w-96 w-fit rounded-lg px-14 py-4 border border-t-white">
+                    <h3 className="text-white border-white">이번 달<br/> 알림 건수</h3>
+                    {alarms.map((data, index) => {
+                        const borderColor = data.borderColor;
+                        const textColor = data.textColor;
 
+                        return (
+                            <div key={index} className="flex items-center gap-3">
+                                <div 
+                                    className={`w-14 h-14 rounded-lg cursor-pointer border flex justify-center items-center text-sm whitespace-pre-line ${textColor} ${borderColor}`}
+                                    title={`${data.label}: ${data.count}`}
+                                >
+                                    {data.label}
+                                </div>
+                                <div
+                                    className={`text-white`}>
+                                    {data.count}건
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+            {/* 아랫 부분 (전 구역 모니터링) */}
+            <div className="flex-1 bg-main-500 p-8 border border-t-white">
+                {/* 전 구역 모니터링 + 서치 바 */}
+                <div className="flex justify-between">
+                    <h1 className="text-2xl font-bold text-white">전 구역 모니터링</h1>
+                    <SearchBar
+                        placeholder="구역을 검색하세요."
+                        iconname="search"
+                    />
+                </div>
             </div>
         </div>
     )
