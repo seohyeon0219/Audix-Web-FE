13b4727943dcb1c9317d3b39f9ecf03bb5e30ff2
Kimseohyeon <seohyeon0219@naver.com>
2025-07-29 00:27:41 +0900

    Fix: focus 효과 제거 및 UI 개선

--- src/components/common/searchBar.tsx
+++ src/components/common/searchBar.tsx
@@ -12,14 +12,14 @@ export default function SearchBar({
     return (
         <div className="flex items-center bg-white w-80 h-12 rounded-3xl">
             <input 
-                className='w-64 h-12 text-login-gray pl-6'
+                className='w-64 h-12 text-login-gray pl-6 outline-none focus:ring-0'
                 placeholder={placeholder}
             ></input>
             <Icon 
                 iconname= {iconname} 
                 size={26} 
                 weight={200} 
-                className="pl-2 text-login-gray"
+                className="pl-2 text-login-gray cursor-pointer"
                 />
         </div>
     )
