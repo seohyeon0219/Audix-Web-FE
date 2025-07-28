d7dcfe32413f23aeb8e1a799f866fb5033fa3d16
Kimseohyeon <seohyeon0219@naver.com>
2025-07-29 00:29:33 +0900

    Feat: AreaCard 상태별 정렬 함수 후 구현

--- src/components/areaMonitoring/areaList.tsx
+++ src/components/areaMonitoring/areaList.tsx
@@ -4,12 +4,27 @@ import { mockAreaCardData } from "./areaCard";
 import AreaCard from "./areaCard";
 
 export default function AreaList() {
+    // 상태별 우선순위
+    const statusPriority = {
+        'danger': 1,
+        'warning': 2,
+        'safe': 3,
+        'offline': 4,
+        'repair': 5
+    }
+
+    // 상태별 정렬
+    const sortedAreas = [...mockAreaCardData].sort((a, b) => {
+        return statusPriority[a.status] - statusPriority[b.status];
+    });
+
     return (
         <div className="grid grid-cols-4 gap-4">
-            {mockAreaCardData.map((area) => (
+            {sortedAreas.map((area, index) => (
                 <AreaCard
                     key={area.id}
                     data={area}
+                    index={index}
                 />
             ))}
         </div>
