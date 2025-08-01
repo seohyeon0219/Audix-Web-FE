6919ec9e53fe19e9f2332a7fd99b186bcdca2eac
Kimseohyeon <seohyeon0219@naver.com>
2025-08-01 11:13:45 +0900

    Feat: 구역 헤더를 장비 상세 페이지에서 제거

--- src/app/area/[id]/layout.tsx
+++ src/app/area/[id]/layout.tsx
@@ -5,28 +5,14 @@ import { mockAreaCardData } from "../../../components/areaMonitoring/areaCard";
 
 interface MapLayoutProps {
     children: React.ReactNode;
-    params: Promise<{ id: string }>;
 }
 
-// id로 구역 이름 찾는 함수
-const getAreaName = (id: string) => {
-    const area = mockAreaCardData.find(area => area.id === parseInt(id));
-    return area?.name;
-}
-
-export default function MapLayout({ children, params }: MapLayoutProps) {
-    const { id } = use(params);
-    const areaName = getAreaName(id);
+export default function MapLayout({ children }: MapLayoutProps) {
     return (
-        <div>
-            {/* 상단 구역명 */}
-            <header className="flex items-center w-full h-20 border-2 border-main-100">
-                <h1 className="border-l-2 border-white text-white text-xl ml-4 pl-8">{areaName} 구조도</h1>
-            </header>
-            {/* 하단 지도 */}
-            <main className="w-full mt-6">
-                {children}
-            </main>
+        <div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+            {children}
         </div>
     )
 }
+
+
--- src/app/area/[id]/page.tsx
+++ src/app/area/[id]/page.tsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react";
 // import AreaCanvas from "../../../components/konva/areaCanvas";
 import { useParams } from "next/navigation";
 import dynamic from "next/dynamic";
+import { mockAreaCardData } from "../../../components/areaMonitoring/areaCard";
+import { use } from "react";
 
 // 동적 import로 AreaCanvas 로드하기 (SSR 비활성화)
 const AreaCanvas = dynamic(() => import ("../../../components/konva/areaCanvas"), {
@@ -15,16 +17,34 @@ const AreaCanvas = dynamic(() => import ("../../../components/konva/areaCanvas")
     )
 });
 
-export default function MapPage() {
-    const params = useParams();
+// id로 구역 이름 찾는 함수
+const getAreaName = (id: string) => {
+    const area = mockAreaCardData.find(area => area.id === parseInt(id));
+    return area?.name;
+}
+
+interface MapPageProps {
+    params: Promise<{ id: string }>;
+}
+
+export default function MapPage({ params }: MapPageProps) {
+    const { id } = use(params);
+    const areaName = getAreaName(id);
 
     return (
         <div>
-            <AreaCanvas 
-                areaId={params.id as string}
-                width={1000}
-                height={500}
-            />
+            {/* 상단 구역명 */}
+            <header className="flex items-center w-full h-20 border-2 border-main-100">
+                <h1 className="border-l-2 border-white text-white text-xl ml-4 pl-8">{areaName} 구조도</h1>
+            </header>
+            {/* 하단 지도 */}
+            <div className="w-full mt-6">
+                <AreaCanvas 
+                    areaId={id}
+                    width={1000}
+                    height={500}
+                />
+            </div>
         </div>
     )
 }
