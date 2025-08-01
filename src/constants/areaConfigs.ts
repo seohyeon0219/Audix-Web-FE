6134536dafa48b4d2faf555b266ce1f316812035
Kimseohyeon <seohyeon0219@naver.com>
2025-08-01 23:21:22 +0900

    Feat: 수치를 통해 장비 상태를 판단하는 함수로 변경

--- src/constants/areaConfigs.ts
+++ src/constants/areaConfigs.ts
@@ -1,11 +1,13 @@
-import { StatusType } from "./statusColor";
+import { machine } from "os";
+import { StatusType, getStatusFromValue } from "./statusColor";
 
 interface AreaConfig {
     machines: Array<{
         id: string;
         x: number;
         y: number;
-        status: StatusType;
+        status?: StatusType;
+        value?: number;
         name: string;
     }>;
     conveyors: Array<{
@@ -16,10 +18,10 @@ interface AreaConfig {
 const AREA_CONFIGS: Record<string, AreaConfig> = {
     'area1' : {
         machines: [
-            { id: '1', name: '기계A', x: 300, y: 200, status: 'safe' },
-            { id: '2', name: '기계B', x: 500, y: 200, status: 'warning' },
-            { id: '3', name: '기계C', x: 700, y: 200, status: 'danger' },
-            { id: '4', name: '로봇팔A', x: 300, y: 400, status: 'safe' },
+            { id: '1', name: '기계A', x: 300, y: 200, value: 0.85 },
+            { id: '2', name: '기계B', x: 500, y: 200, value: 0.45 },
+            { id: '3', name: '기계C', x: 700, y: 200, value: 0.15 },
+            { id: '4', name: '로봇팔A', x: 300, y: 400, value: 0.92 },
             { id: '5', name: '로봇팔B', x: 500, y: 400, status: 'offline' },
             { id: '6', name: '로봇팔C', x: 700, y: 400, status: 'repair' },
         ],
@@ -31,10 +33,10 @@ const AREA_CONFIGS: Record<string, AreaConfig> = {
     },
     'area2' : {
         machines: [
-            { id: '1', name: '기계C', x: 200, y: 200, status: 'safe' },
-            { id: '2', name: '기계B', x: 200, y: 100, status: 'warning' },
-            { id: '3', name: '기계A', x: 300, y: 100, status: 'danger' },
-            { id: '4', name: '로봇팔A', x: 150, y: 200, status: 'safe' },
+            { id: '1', name: '기계C', x: 200, y: 200, value: 0.92 },
+            { id: '2', name: '기계B', x: 200, y: 100, value: 0.45 },
+            { id: '3', name: '기계A', x: 300, y: 100, value: 0.85 },
+            { id: '4', name: '로봇팔A', x: 150, y: 200, value: 0.15 },
             { id: '5', name: '로봇팔B', x: 250, y: 200, status: 'offline' },
             { id: '6', name: '로봇팔C', x: 350, y: 200, status: 'repair' },
         ],
@@ -49,4 +51,16 @@ const AREA_CONFIGS: Record<string, AreaConfig> = {
 // 구역 ID에 따라 해당 구역의 설정 데이터를 반환하는 함수
 export const getAreaConfig = (areaId: string) => {
     return AREA_CONFIGS[areaId] || AREA_CONFIGS['area1']
+}
+
+// 장비의 현재 상태를 반환하는 함수
+export const getMachineStatus = (machine: AreaConfig['machines'][0]): StatusType => {
+    if (machine.status) {
+        return machine.status;
+    }
+
+    if (machine.value !== undefined) {
+        return getStatusFromValue(machine.value);
+    }
+    return 'warning';
 }
