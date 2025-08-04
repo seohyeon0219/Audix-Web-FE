02b4c4a34bfb40f8c4b4c960fbf292c061e31065
Kimseohyeon <seohyeon0219@naver.com>
2025-08-04 23:04:23 +0900

    Feat: 검색창 기능 제거

--- src/app/area/page.tsx
+++ src/app/area/page.tsx
@@ -15,13 +15,9 @@ export default function AreaPage() {
             </div>
             {/* 아랫 부분 (전 구역 모니터링) */}
             <div className="flex-1 bg-main-500 p-6 border border-t-white">
-                {/* 전 구역 모니터링 + 서치 바 */}
-                <div className="flex justify-between mb-6">
+                {/* 전 구역 모니터링 */}
+                <div className="mb-6">
                     <h1 className="text-white">전 구역 모니터링</h1>
-                    <SearchBar
-                        placeholder="구역을 검색하세요."
-                        iconname="search"
-                    />
                 </div>
                 {/* 구역 카드 리스트 */}
                 <AreaList />
