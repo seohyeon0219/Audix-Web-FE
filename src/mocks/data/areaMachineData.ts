665910ffe93929ecae5224950e207ceb3736e3b0
Kimseohyeon <seohyeon0219@naver.com>
2025-08-04 11:14:23 +0900

    Feat: 구역 데이터에 공장명 추가

--- src/mocks/data/areaMachineData.ts
+++ src/mocks/data/areaMachineData.ts
@@ -3,14 +3,15 @@ import { MachineData, AreaData } from '@/types/areaMachineType';
 // 구역별 장비 mock data
 const areaA1: AreaData = {
     id: '1',
-    name: '울산 제1공장',
+    name: 'A-1구역',
+    location: '울산 제1공장',
     manager: '김서현',
     machines : [
         {
             id: 'A1-M001',
             name: 'Robot Arm',
             model: 'R-2000iC/165F',
-            location: '울산 제1공장 1층',
+            location: '프레스 공정 (Press Process)',
             manager: '김재걸',
             parts: ['Pulse coder', 'moter'],
             value: 0.85
@@ -19,7 +20,7 @@ const areaA1: AreaData = {
             id: 'A1-M002',
             name: 'Robot Arm',
             model: 'R-2000iC/210F',
-            location: '울산 제1공장 1층',
+            location: '프레스 공정 (Press Process)',
             manager: '김재걸',
             parts: ['Pulse coder', 'moter'],
             value: 0.8
@@ -28,7 +29,7 @@ const areaA1: AreaData = {
             id: 'A1-M003',
             name: 'Robot Arm',
             model: 'R-2000iC/210L',
-            location: '울산 제1공장 1층',
+            location: '프레스 공정 (Press Process)',
             manager: '김재걸',
             parts: ['Pulse coder', 'moter'],
             value: 0.8
@@ -37,7 +38,7 @@ const areaA1: AreaData = {
             id: 'A1-M004',
             name: 'Robot Arm',
             model: 'R-2000iC/100P',
-            location: '울산 제1공장 1층',
+            location: '프레스 공정 (Press Process)',
             manager: '김재걸',
             parts: ['Pulse coder', 'moter'],
             value: 0.7
@@ -46,7 +47,7 @@ const areaA1: AreaData = {
             id: 'A1-M005',
             name: 'Robot Arm',
             model: 'R-2000iC/125L',
-            location: '울산 제1공장 1층',
+            location: '프레스 공정 (Press Process)',
             manager: '김재걸',
             parts: ['Pulse coder', 'moter'],
             value: 0.9
@@ -55,7 +56,7 @@ const areaA1: AreaData = {
             id: 'A1-M006',
             name: 'Robot Arm',
             model: 'R-2000iC/165R',
-            location: '울산 제1공장 1층',
+            location: '프레스 공정 (Press Process)',
             manager: '김재걸',
             parts: ['Pulse coder', 'moter'],
             value: 0.8
@@ -65,14 +66,15 @@ const areaA1: AreaData = {
 
 const areaA2: AreaData = {
     id: '2',
-    name: '울산 제2공장',
+    name: 'A-2구역',
+    location: '울산 제1공장',
     manager: '김재걸',
     machines : [
         {
             id: 'A2-M001',
             name: 'Robot Arm',
             model: 'R-2000iC/165F',
-            location: '울산 제2공장 1층',
+            location: '차체 공정 (BIW Welding Process)',
             manager: '이하은',
             parts: ['Pulse coder', 'moter'],
             value: 0.7
@@ -81,7 +83,7 @@ const areaA2: AreaData = {
             id: 'A2-M002',
             name: 'Robot Arm',
             model: 'R-2000iC/210F',
-            location: '울산 제2공장 1층',
+            location: '차체 공정 (BIW Welding Process)',
             manager: '이하은',
             parts: ['Pulse coder', 'moter'],
             value: 0.9
@@ -90,7 +92,7 @@ const areaA2: AreaData = {
             id: 'A2-M003',
             name: 'Robot Arm',
             model: 'R-2000iC/210L',
-            location: '울산 제2공장 1층',
+            location: '차체 공정 (BIW Welding Process)',
             manager: '이하은',
             parts: ['Pulse coder', 'moter'],
             value: 0.8
@@ -99,7 +101,7 @@ const areaA2: AreaData = {
             id: 'A2-M004',
             name: 'Robot Arm',
             model: 'R-2000iC/100P',
-            location: '울산 제2공장 1층',
+            location: '차체 공정 (BIW Welding Process)',
             manager: '이하은',
             parts: ['Pulse coder', 'moter'],
             value: 0.9
@@ -108,7 +110,7 @@ const areaA2: AreaData = {
             id: 'A2-M005',
             name: 'Robot Arm',
             model: 'R-2000iC/125L',
-            location: '울산 제2공장 1층',
+            location: '차체 공정 (BIW Welding Process)',
             manager: '이하은',
             parts: ['Pulse coder', 'moter'],
             value: 0.8
@@ -117,7 +119,7 @@ const areaA2: AreaData = {
             id: 'A2-M006',
             name: 'Robot Arm',
             model: 'R-2000iC/165R',
-            location: '울산 제2공장 1층',
+            location: '차체 공정 (BIW Welding Process)',
             manager: '이하은',
             parts: ['Pulse coder', 'moter'],
             value: 0.9
@@ -127,14 +129,15 @@ const areaA2: AreaData = {
 
 const areaA3: AreaData = {
     id: '3',
-    name: '울산 제3공장',
+    name: 'A-3구역',
+    location: '울산 제3공장',
     manager: '이하은',
     machines : [
         {
             id: 'A3-M001',
             name: 'Robot Arm',
             model: 'R-2000iC/165F',
-            location: '울산 제3공장 1층',
+            location: '도장 공정 (Painting Process)',
             manager: '김현민',
             parts: ['Pulse coder', 'moter'],
             value: 0.6
@@ -143,7 +146,7 @@ const areaA3: AreaData = {
             id: 'A3-M002',
             name: 'Robot Arm',
             model: 'R-2000iC/210F',
-            location: '울산 제3공장 1층',
+            location: '도장 공정 (Painting Process)',
             manager: '김현민',
             parts: ['Pulse coder', 'moter'],
             value: 0.5
@@ -152,7 +155,7 @@ const areaA3: AreaData = {
             id: 'A3-M003',
             name: 'Robot Arm',
             model: 'R-2000iC/210L',
-            location: '울산 제3공장 1층',
+            location: '도장 공정 (Painting Process)',
             manager: '김현민',
             parts: ['Pulse coder', 'moter'],
             value: 0.6
@@ -161,7 +164,7 @@ const areaA3: AreaData = {
             id: 'A3-M004',
             name: 'Robot Arm',
             model: 'R-2000iC/100P',
-            location: '울산 제3공장 1층',
+            location: '도장 공정 (Painting Process)',
             manager: '김현민',
             parts: ['Pulse coder', 'moter'],
             value: 0.5
@@ -170,7 +173,7 @@ const areaA3: AreaData = {
             id: 'A3-M005',
             name: 'Robot Arm',
             model: 'R-2000iC/125L',
-            location: '울산 제3공장 1층',
+            location: '도장 공정 (Painting Process)',
             manager: '김현민',
             parts: ['Pulse coder', 'moter'],
             value: 0.6
@@ -179,7 +182,7 @@ const areaA3: AreaData = {
             id: 'A3-M006',
             name: 'Robot Arm',
             model: 'R-2000iC/165R',
-            location: '울산 제3공장 1층',
+            location: '도장 공정 (Painting Process)',
             manager: '김현민',
             parts: ['Pulse coder', 'moter'],
             value: 0.5
@@ -189,14 +192,15 @@ const areaA3: AreaData = {
 
 const areaA4: AreaData = {
     id: '4',
-    name: '울산 제4공장',
+    name: 'A-4구역',
+    location: '울산 제4공장',
     manager: '김현민',
     machines : [
         {
             id: 'A4-M001',
             name: 'Robot Arm',
             model: 'R-2000iC/165F',
-            location: '울산 제4공장 1층',
+            location: '조립공정 (Assembly Process)',
             manager: '도종명',
             parts: ['Pulse coder', 'moter'],
             value: 0.6
@@ -205,7 +209,7 @@ const areaA4: AreaData = {
             id: 'A4-M002',
             name: 'Robot Arm',
             model: 'R-2000iC/210F',
-            location: '울산 제4공장 1층',
+            location: '조립공정 (Assembly Process)',
             manager: '도종명',
             parts: ['Pulse coder', 'moter'],
             value: 0.8
@@ -214,7 +218,7 @@ const areaA4: AreaData = {
             id: 'A4-M003',
             name: 'Robot Arm',
             model: 'R-2000iC/210L',
-            location: '울산 제4공장 1층',
+            location: '조립공정 (Assembly Process)',
             manager: '도종명',
             parts: ['Pulse coder', 'moter'],
             value: 0.4
@@ -223,7 +227,7 @@ const areaA4: AreaData = {
             id: 'A4-M004',
             name: 'Robot Arm',
             model: 'R-2000iC/100P',
-            location: '울산 제4공장 1층',
+            location: '조립공정 (Assembly Process)',
             manager: '도종명',
             parts: ['Pulse coder', 'moter'],
             value: 0.6
@@ -232,7 +236,7 @@ const areaA4: AreaData = {
             id: 'A4-M005',
             name: 'Robot Arm',
             model: 'R-2000iC/125L',
-            location: '울산 제4공장 1층',
+            location: '조립공정 (Assembly Process)',
             manager: '도종명',
             parts: ['Pulse coder', 'moter'],
             value: 0.5
@@ -241,7 +245,7 @@ const areaA4: AreaData = {
             id: 'A4-M006',
             name: 'Robot Arm',
             model: 'R-2000iC/165R',
-            location: '울산 제4공장 1층',
+            location: '조립공정 (Assembly Process)',
             manager: '도종명',
             parts: ['Pulse coder', 'moter'],
             value: 0.5
@@ -249,73 +253,72 @@ const areaA4: AreaData = {
     ]
 }
 
-const areaA5: AreaData = {
-    id: '5',
-    name: '울산 제5공장',
-    manager: '도종명',
-    machines : [
-        {
-            id: 'A5-M001',
-            name: 'Robot Arm',
-            model: 'R-2000iC/165F',
-            location: '울산 제5공장 1층',
-            manager: '김서현',
-            parts: ['Pulse coder', 'moter'],
-            value: 0.85
-        },
-        {
-            id: 'A5-M002',
-            name: 'Robot Arm',
-            model: 'R-2000iC/210F',
-            location: '울산 제5공장 1층',
-            manager: '김서현',
-            parts: ['Pulse coder', 'moter'],
-            value: 0.25
-        },
-        {
-            id: 'A5-M003',
-            name: 'Robot Arm',
-            model: 'R-2000iC/210L',
-            location: '울산 제5공장 1층',
-            manager: '김서현',
-            parts: ['Pulse coder', 'moter'],
-            value: 0.35
-        },
-        {
-            id: 'A5-M004',
-            name: 'Robot Arm',
-            model: 'R-2000iC/100P',
-            location: '울산 제5공장 1층',
-            manager: '김서현',
-            parts: ['Pulse coder', 'moter'],
-            value: 0.7
-        },
-        {
-            id: 'A5-M005',
-            name: 'Robot Arm',
-            model: 'R-2000iC/125L',
-            location: '울산 제5공장 1층',
-            manager: '김서현',
-            parts: ['Pulse coder', 'moter'],
-            value: 0.1
-        },
-        {
-            id: 'A5-M006',
-            name: 'Robot Arm',
-            model: 'R-2000iC/165R',
-            location: '울산 제5공장 1층',
-            manager: '김서현',
-            parts: ['Pulse coder', 'moter'],
-            value: 0.9
-        }
-    ]
-}
+// const areaA5: AreaData = {
+//     id: '5',
+//     name: '울산 제5공장',
+//     manager: '도종명',
+//     machines : [
+//         {
+//             id: 'A5-M001',
+//             name: 'Robot Arm',
+//             model: 'R-2000iC/165F',
+//             location: '울산 제5공장 1층',
+//             manager: '김서현',
+//             parts: ['Pulse coder', 'moter'],
+//             value: 0.85
+//         },
+//         {
+//             id: 'A5-M002',
+//             name: 'Robot Arm',
+//             model: 'R-2000iC/210F',
+//             location: '울산 제5공장 1층',
+//             manager: '김서현',
+//             parts: ['Pulse coder', 'moter'],
+//             value: 0.25
+//         },
+//         {
+//             id: 'A5-M003',
+//             name: 'Robot Arm',
+//             model: 'R-2000iC/210L',
+//             location: '울산 제5공장 1층',
+//             manager: '김서현',
+//             parts: ['Pulse coder', 'moter'],
+//             value: 0.35
+//         },
+//         {
+//             id: 'A5-M004',
+//             name: 'Robot Arm',
+//             model: 'R-2000iC/100P',
+//             location: '울산 제5공장 1층',
+//             manager: '김서현',
+//             parts: ['Pulse coder', 'moter'],
+//             value: 0.7
+//         },
+//         {
+//             id: 'A5-M005',
+//             name: 'Robot Arm',
+//             model: 'R-2000iC/125L',
+//             location: '울산 제5공장 1층',
+//             manager: '김서현',
+//             parts: ['Pulse coder', 'moter'],
+//             value: 0.1
+//         },
+//         {
+//             id: 'A5-M006',
+//             name: 'Robot Arm',
+//             model: 'R-2000iC/165R',
+//             location: '울산 제5공장 1층',
+//             manager: '김서현',
+//             parts: ['Pulse coder', 'moter'],
+//             value: 0.9
+//         }
+//     ]
+// }
 
 // 전체 구역 데이터 배열
 export const mockAreaMachineData: AreaData[] = [
     areaA1,
     areaA2,
     areaA3,
-    areaA4,
-    areaA5
+    areaA4
 ];
